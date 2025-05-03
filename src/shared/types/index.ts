c48e7a7c079f252c4f1f63328148624e2d3218e7
annetannet <annetpeach@example.com>
2025-05-04 00:51:38 +0500

    add offer-generator

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,3 +6,5 @@ export * from './housing-type.enum.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './user-type.enum.js';
+export * from './mock-server-data.type.js';
+export * from './offer-type.enum.js';
