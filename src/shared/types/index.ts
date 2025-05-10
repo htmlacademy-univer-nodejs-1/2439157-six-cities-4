b7ba8fabfcae5c724c043902571cbe1a58c0bb81
annetannet <annetpeach@example.com>
2025-05-11 02:41:30 +0500

    db added

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,4 +3,4 @@ export * from './user.type.js';
 export * from './mock-server-data.type.js';
 export * from './offer-type.enum.js';
 export * from './component.enum.js';
-
+export * from './category.type.js';
